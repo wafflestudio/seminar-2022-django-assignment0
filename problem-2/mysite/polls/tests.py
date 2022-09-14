279c095be5e7a018777ddfccb9633c85da9c5b62
jang1751 <jang980918@gmail.com>
2022-09-15 02:07:14 +0900

    django tutorial 완료

--- problem-2/mysite/polls/tests.py
+++ problem-2/mysite/polls/tests.py
@@ -4,11 +4,10 @@ from django.test import TestCase
 from django.utils import timezone
 from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 # Create your tests here.
 
 class QuestionModelTests(TestCase):
-
     def test_was_published_recently_with_future_question(self):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
@@ -24,9 +23,10 @@ class QuestionModelTests(TestCase):
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
 
-def create_question(question_text, days):
+def create_question(question_text, days=0):
     time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date=time)
+
 
 class QuestionIndexViewTests(TestCase):
     def test_no_question(self):
@@ -67,6 +67,31 @@ class QuestionIndexViewTests(TestCase):
             [question2, question1],
         )
 
+    # def test_no_choice_question(self):
+    #     question = create_question(question_text="No Choice Question.")
+    #     response = self.client.get(reverse("polls:index"))
+    #     self.assertQuerysetEqual(
+    #     response.context['latest_question_list'],
+    #     []
+    #     )
+    #
+    # def test_with_choices_question(self):
+    #     question = create_question(question_text="With Choice Question.")
+    #     response = self.client.get(reverse("polls:index"))
+    #     self.assertQuerysetEqual(
+    #         response.context['latest_question_list'],
+    #         [question]
+    #     )
+    #
+    # def test_no_choice_and_with_choices_question(self):
+    #     question= create_question(question_text="With Choices Question.")
+    #     create_question(question_text="No Choices Question.")
+    #     response = self.client.get(reverse('polls:index'))
+    #     self.assertQuerysetEqual(
+    #         response.context['latest_question_list'],
+    #         [question],
+    #     )
+
 class QuestionDetailViewTests(TestCase):
     def test_future_question(self):
         future_question = create_question(question_text='Future question.', days=5)
@@ -79,3 +104,41 @@ class QuestionDetailViewTests(TestCase):
         url = reverse('polls:detail', args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
+
+class QuestionResultsViewTests(TestCase):
+    def test_future_question(self):
+        future_question = create_question(question_text='Future Question.', days=30)
+        url = reverse('polls:results', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        past_question = create_question(question_text='Past Question.', days=-5)
+        url = reverse('polls:results', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
+    def test_no_choice_question(self):
+        no_choice_question = create_question(question_text='No Choice Question.')
+        url = reverse('polls:results', args=(no_choice_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, no_choice_question.question_text)
+
+# class AdminUserTests(TestCase):
+
+# class ChoiceModelTests(TestCase):
+#     def test_minus_votes(self):
+#         #question 생성
+#         #choice votes 음수로 생성
+#         time = timezone.now()
+#         question = Question(pub_date=time)
+#         minus_votes_choice = Choice(question, choice_text="minus votes choice", votes=-100)
+#         self.assertIs(minus_votes_choice.is_minus_votes(), False)
+#
+#     def test_not_having_question(self):
+#         # 질문이 없는 선택지 생성 테스트
+#         no_question_choice = Choice(None, choice_text="No Question Choice.", votes=0)
+#         self.assertIs(no_question_choice.has_question(), False)
+#
+# def create_choice(question, choice_text, votes):
+#     return Choice.objects.create(question=question, choice_text=choice_text, votes=votes)
--- problem-2/mysite/polls/views.py
+++ problem-2/mysite/polls/views.py
@@ -20,13 +20,12 @@ class DetailView(generic.DeleteView):
     def get_queryset(self):
         return Question.objects.filter(pub_date__lte=timezone.now())
 
-# def results(request, question_id):
-#     question = get_object_or_404(Question, pk = question_id)
-#     return render(request, 'polls/results.html', {'question': question})
-
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
