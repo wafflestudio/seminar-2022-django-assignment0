3c4d0a9a6d1ab65e0cc7e887533b8578d792750e
sungmin <sungsung718@gmail.com>
2022-09-16 22:51:04 +0900

    problem2 tutorial 6
    tutorial 5을 하는 도중 views.py에서 빠뜨린 코드 또한 추가하였음.

--- problem_2/mysite/polls/views.py
+++ problem_2/mysite/polls/views.py
@@ -22,7 +22,8 @@ class IndexView(generic.ListView):
 
 
 class DetailView(generic.DetailView):
-    ...
+    model = Question
+    template_name = 'polls/detail.html'
     def get_queryset(self):
         """
         Excludes any questions that aren't published yet.
