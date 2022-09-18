55f8291c9c2d73b85af8084dd4d28df5fe37f6d8
choco-bear <kdy030529@gmail.com>
2022-09-18 19:12:04 +0900

    Finished react tutorial pt4

--- problem-3/reactTutorial/snippets/serializers.py
+++ problem-3/reactTutorial/snippets/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
@@ -30,6 +31,8 @@ class SnippetSerializer(serializers.Serializer):
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Snippet
         fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
