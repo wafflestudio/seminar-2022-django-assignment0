56abbab1fb2669ae5f23e7e80fe73885a96bae3d
dlatn4389 <76550906+dlatn4389@users.noreply.github.com>
2022-09-19 03:39:00 +0900

    ~3

--- problem-3/tutorial/snippets/views.py
+++ problem-3/tutorial/snippets/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
