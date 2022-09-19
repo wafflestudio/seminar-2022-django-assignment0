53ef7deda8fe79758bbe8542b64ba64ec0e33c1f
reynie1514 <reynie1514@naver.com>
2022-09-19 13:50:55 +0900

    DRF4 Authentication & Permissions

--- problem-3/tutorial/snippets/views.py
+++ problem-3/tutorial/snippets/views.py
@@ -1,20 +1,34 @@
-from django.http import Http404
-from rest_framework import status, mixins, generics
-from rest_framework.views import APIView
+from rest_framework import status, generics, permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.contrib.auth.models import User
 
 from .models import Snippet
-from .serializers import SnippetSerializer
+from .serializers import SnippetSerializer, UserSerializer
+from .permissions import IsOwnerOrReadOnly
 
 # Class-Based Views (Generic)
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 # Function Based Views
 @api_view(['GET', 'POST'])
