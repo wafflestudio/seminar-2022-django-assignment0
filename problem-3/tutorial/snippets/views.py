419bdf84423fffdea49d0e357e216c0833882f71
reynie1514 <reynie1514@naver.com>
2022-09-19 13:09:26 +0900

    DRF3 Class-Based-Views

--- problem-3/tutorial/snippets/views.py
+++ problem-3/tutorial/snippets/views.py
@@ -1,9 +1,22 @@
-from rest_framework import status
+from django.http import Http404
+from rest_framework import status, mixins, generics
+from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
 from .models import Snippet
 from .serializers import SnippetSerializer
 
+# Class-Based Views (Generic)
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+# Function Based Views
 @api_view(['GET', 'POST'])
 def snippet_list(request, format=None):
     """
