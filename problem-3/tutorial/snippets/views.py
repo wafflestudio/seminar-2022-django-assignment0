416e45a0f82d4afc191c338062a57727a3333f8b
reynie1514 <reynie1514@naver.com>
2022-09-19 14:28:03 +0900

    DRF5 Relationships & Hyperlinked APIs

--- problem-3/tutorial/snippets/views.py
+++ problem-3/tutorial/snippets/views.py
@@ -1,11 +1,27 @@
-from rest_framework import status, generics, permissions
+from rest_framework import status, generics, permissions, renderers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from django.contrib.auth.models import User
 
 from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
 from .permissions import IsOwnerOrReadOnly
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format),
+    })
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 # Class-Based Views (Generic)
 class SnippetList(generics.ListCreateAPIView):
